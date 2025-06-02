890a75b525e578ac96ce2dc3e87fee77ce0fd0b2
Andrew smogunov <a.batman2003@yandex.ru>
2025-06-02 09:00:02 +0300

    Использовал аннотацию @Data библиотеки Lombok

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,52 +1,13 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.Data;
 import java.time.LocalDate;
 
+@Data
 public class Film {
     private Long id;
     private String name;
     private String description;
     private LocalDate releaseDate;
     private int duration;
-
-    // Геттеры и сеттеры
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDate getReleaseDate() {
-        return releaseDate;
-    }
-
-    public void setReleaseDate(LocalDate releaseDate) {
-        this.releaseDate = releaseDate;
-    }
-
-    public int getDuration() {
-        return duration;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
 }
